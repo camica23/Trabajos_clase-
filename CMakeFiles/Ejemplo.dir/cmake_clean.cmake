f079d7a79acb20c2fa5f341162337c2f046a103b
vamos <laura.andrade-c@mail.escuelaing.edu.co>
2025-03-09 17:23:59 -0500

    trabajos

---
+++ CMakeFiles/Ejemplo.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ejemplo.dir/apuntes.cpp.o"
+  "CMakeFiles/Ejemplo.dir/apuntes.cpp.o.d"
+  "Ejemplo"
+  "Ejemplo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ejemplo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
